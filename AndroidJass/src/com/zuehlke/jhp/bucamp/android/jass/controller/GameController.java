1ac74825a2a6c68ead9fef6f9186c6dd2c944b18
MBaumeler <=github@mbaumelerch>
2012-09-13 15:21:43 +0200

    Nullpointerfix

--- AndroidJass/src/com/zuehlke/jhp/bucamp/android/jass/controller/GameController.java
+++ AndroidJass/src/com/zuehlke/jhp/bucamp/android/jass/controller/GameController.java
@@ -7,6 +7,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import android.os.Handler;
+import ch.mbaumeler.jass.core.Match;
 import ch.mbaumeler.jass.core.card.Card;
 import ch.mbaumeler.jass.core.game.PlayerToken;
 import ch.mbaumeler.jass.extended.ai.PlayStrategy;
@@ -83,10 +84,16 @@ public class GameController implements JassModelObserver {
 	}
 
 	public void playCard() {
-		PlayerToken token = this.game.getCurrentMatch().getActivePlayer();
+		Match currentMatch = this.game.getCurrentMatch();
+		PlayerToken token = currentMatch.getActivePlayer();
 		PlayStrategy strategy = getStrategyForPlayerToken(token);
-		Card cardToPlay = strategy.getCardToPlay(this.game.getCurrentMatch());
-		this.game.getCurrentMatch().playCard(cardToPlay);
+		
+		if (currentMatch.getAnsage() == null) {
+			currentMatch.setAnsage(new SimpleStrategyEngine().create().getAnsage(currentMatch));
+		}
+		
+		Card cardToPlay = strategy.getCardToPlay(currentMatch);
+		currentMatch.playCard(cardToPlay);
 	}
 
 	private PlayStrategy getStrategyForPlayerToken(PlayerToken token) {
